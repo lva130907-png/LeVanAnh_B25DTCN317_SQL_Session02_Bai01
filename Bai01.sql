8f4ff5a2c59a4e09f59f638a4b49962cf6e0a3d7
Lê Vân Anh <lva130907@gmail.com>
2026-04-14 21:26:28 +0700

    Update Bai01.sql

--- Bai01.sql
+++ Bai01.sql
@@ -1,1 +1,9 @@
-SELECT * FROM sesion02_bai01.products;
+CREATE DATABASE session02_bai01;
+USE session02_bai01;
+
+CREATE TABLE PRODUCTS(
+	ID INT PRIMARY KEY,
+    ProductName VARCHAR(120),
+    Price DECIMAL(18,8),
+    Description TEXT
+);
